7de5a92e0375c57cba927eaf474f3601c02b5991
SolanllyCabral <nayelyt3030@gmail.com>
2022-03-18 22:43:27 -0300

    Agregar archivos de proyecto.

---
+++ FormularioArbol.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace ArbolBinario
+{
+    partial class FormularioArbol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnPreorden = new System.Windows.Forms.Button();
+            this.btnPostorden = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInorden = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblNumNod = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(1169, 139);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(125, 27);
+            this.txtDato.TabIndex = 22;
+            this.txtDato.TextChanged += new System.EventHandler(this.txtDato_TextChanged);
+            this.txtDato.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDato_KeyPress);
+            // 
+            // btnPreorden
+            // 
+            this.btnPreorden.FlatAppearance.BorderSize = 0;
+            this.btnPreorden.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnPreorden.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPreorden.Location = new System.Drawing.Point(955, 440);
+            this.btnPreorden.Name = "btnPreorden";
+            this.btnPreorden.Size = new System.Drawing.Size(123, 56);
+            this.btnPreorden.TabIndex = 18;
+            this.btnPreorden.Text = "PreOrden";
+            this.btnPreorden.UseVisualStyleBackColor = true;
+            this.btnPreorden.Click += new System.EventHandler(this.btnPreorden_Click_1);
+            // 
+            // btnPostorden
+            // 
+            this.btnPostorden.FlatAppearance.BorderSize = 0;
+            this.btnPostorden.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnPostorden.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPostorden.Location = new System.Drawing.Point(1269, 440);
+            this.btnPostorden.Name = "btnPostorden";
+            this.btnPostorden.Size = new System.Drawing.Size(123, 56);
+            this.btnPostorden.TabIndex = 20;
+            this.btnPostorden.Text = "PostOrden";
+            this.btnPostorden.UseVisualStyleBackColor = true;
+            this.btnPostorden.Click += new System.EventHandler(this.btnPostorden_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(1032, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 43);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Árbol Binario";
+            // 
+            // btnInorden
+            // 
+            this.btnInorden.FlatAppearance.BorderSize = 0;
+            this.btnInorden.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnInorden.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnInorden.Location = new System.Drawing.Point(1107, 440);
+            this.btnInorden.Name = "btnInorden";
+            this.btnInorden.Size = new System.Drawing.Size(123, 56);
+            this.btnInorden.TabIndex = 19;
+            this.btnInorden.Text = "InOrden";
+            this.btnInorden.UseVisualStyleBackColor = true;
+            this.btnInorden.Click += new System.EventHandler(this.btnInorden_Click_1);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBuscar.Location = new System.Drawing.Point(1001, 249);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(123, 56);
+            this.btnBuscar.TabIndex = 17;
+            this.btnBuscar.Text = "Buscar Nodo";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAltura.Location = new System.Drawing.Point(1160, 369);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(0, 24);
+            this.lblAltura.TabIndex = 21;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnInsertar.FlatAppearance.BorderSize = 0;
+            this.btnInsertar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInsertar.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnInsertar.Location = new System.Drawing.Point(1001, 109);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(123, 56);
+            this.btnInsertar.TabIndex = 14;
+            this.btnInsertar.Text = "Insertar Nodo";
+            this.btnInsertar.UseVisualStyleBackColor = false;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click_1);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btnEliminar.FlatAppearance.BorderSize = 0;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEliminar.Location = new System.Drawing.Point(1001, 175);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(123, 56);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "Eliminar Nodo";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click_1);
+            // 
+            // lblNumNod
+            // 
+            this.lblNumNod.AutoSize = true;
+            this.lblNumNod.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNumNod.Location = new System.Drawing.Point(1001, 369);
+            this.lblNumNod.Name = "lblNumNod";
+            this.lblNumNod.Size = new System.Drawing.Size(0, 24);
+            this.lblNumNod.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(1079, 369);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(1269, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 24);
+            this.label3.TabIndex = 25;
+            // 
+            // FormularioArbol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 549);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNumNod);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.btnPreorden);
+            this.Controls.Add(this.btnPostorden);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInorden);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnEliminar);
+            this.Name = "FormularioArbol";
+            this.Text = "FormularioArbol";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormularioArbol_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FormularioArbol_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnPreorden;
+        private System.Windows.Forms.Button btnPostorden;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInorden;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnEliminar;
+        public System.Windows.Forms.Label lblNumNod;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ FormularioArbol.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArbolBinario
+{    
+    public partial class FormularioArbol : Form
+    {
+        
+        public FormularioArbol()
+        {
+            InitializeComponent();
+        }
+
+        //Desabilitadores y habilitadores
+        protected void habilitarButtons()
+        {
+            btnPreorden.Enabled = true;
+            btnPostorden.Enabled = true;
+            btnInorden.Enabled = true;
+        }
+
+        protected void deshabilitarButtons()
+        {
+            btnPreorden.Enabled = false;
+            btnPostorden.Enabled = false;
+            btnInorden.Enabled = false;
+        }
+
+        //Declaracion de variables a utilizar
+        int Dato = 0;
+        int cont = 0;
+        int cot=0;
+        int num = 0;
+        int hoja = 0;
+        int resta = 0;
+        int suma = 0;
+        int A=0, B=0;
+        int altura = 0;
+        ArbolBinario mi_Arbol = new ArbolBinario(null);  //Creacion del objeto Arbol
+        ArbolBinario mi_nodo = new ArbolBinario(null);
+        Graphics g;     // Definion del objeto grafico
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Evento del formulario que permitirá dibujar el Arbol binario
+        private void FormularioArbol_Paint(object sender, PaintEventArgs en)
+        {
+            en.Graphics.Clear(this.BackColor);
+            en.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            en.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g = en.Graphics;
+
+            
+            mi_Arbol.DibujarArbol(g, this.Font, Brushes.Salmon, Brushes.Black, Pens.Black, Brushes.White);
+            
+        }
+
+        private void btnInsertar_Click_1(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtDato.Text))
+            {
+                MessageBox.Show("El árbol esta vacío debe colocar un dato");
+                txtDato.Focus();
+                return;
+
+            }
+            else
+            {
+                
+                Dato = int.Parse(txtDato.Text);
+                mi_Arbol.Insertar(Dato);
+                if (cot != Dato)
+                {   
+                    
+                    cot = Dato;
+                    txtDato.Clear();
+                    txtDato.Focus();
+                    num++;
+                    lblAltura.Text = "Altura_A: " + mi_Arbol.calcularAltura(mi_Arbol.Raiz);
+                    
+                    lblAltura.Visible = true;
+
+                    if (num > 1)
+                    {
+                        
+                        A = mi_Arbol.calcularAltura(mi_Arbol.Raiz);
+                        resta = num - A;
+                       
+                    }
+                    lblNumNod.Text = "Nodo: " + Convert.ToString(num);
+                    lblNumNod.Visible = true;
+                    label2.Text = "Hoja: " + Convert.ToString(resta); 
+                    label2.Visible = true;
+
+                    if (A > 0)
+                    {
+                        suma = 1+mi_Arbol.calcularAltura(mi_Arbol.Raiz);
+                    }
+                    label3.Text = "Altura_N: " + Convert.ToString(suma);
+                    label3.Visible = true;
+                    habilitarButtons();
+                    Refresh();
+                }
+                else { resta = resta - 1;
+
+                    if (A > 0)
+                    {
+                        suma = 1 + mi_Arbol.calcularAltura(mi_Arbol.Raiz);
+                    }
+                    label3.Text = "Altura_N: " + Convert.ToString(suma);
+                    label3.Visible = true;
+
+
+
+                }
+                        
+                        
+                        
+            }
+        }
+
+        private void btnEliminar_Click_1(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtDato.Text))
+            {
+                MessageBox.Show("No existe datos que eliminar");
+                txtDato.Focus();
+                return;
+
+            }
+            else
+            {
+                Dato = int.Parse(txtDato.Text);
+                mi_Arbol.Eliminar(Dato);
+
+                txtDato.Clear();
+                txtDato.Focus();
+
+                cont--;
+                num--;
+                if (cont == 0)
+                {
+                    deshabilitarButtons();
+                    lblAltura.Visible = false;
+                }
+
+                Refresh();
+            }
+        }
+
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtDato.Text))
+            {
+                MessageBox.Show("No existe datos que buscar");
+                txtDato.Focus();
+                return;
+
+            }
+            else
+            {
+                Dato = int.Parse(txtDato.Text);
+
+                mi_Arbol.Buscar(Dato);
+
+                txtDato.Clear();
+                txtDato.Focus();
+
+                Refresh();
+            }
+        }
+
+        private void btnPreorden_Click_1(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            Refresh();
+            txtDato.Clear();
+            deshabilitarButtons();
+            g = this.CreateGraphics();
+            mi_Arbol.colorear(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, mi_Arbol.Raiz, false, false, true);
+            habilitarButtons();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void btnInorden_Click_1(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            Refresh();
+            txtDato.Clear();
+            deshabilitarButtons();
+            g = this.CreateGraphics();
+            mi_Arbol.colorear(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, mi_Arbol.Raiz, false, true, false);
+            habilitarButtons();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void btnPostorden_Click_1(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            Refresh();
+            txtDato.Clear();
+            deshabilitarButtons();
+            g = this.CreateGraphics();
+            mi_Arbol.colorear(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, mi_Arbol.Raiz, true, false, false);
+            habilitarButtons();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void FormularioArbol_MouseClick(object sender, MouseEventArgs e)
+        {
+            int valor = 100;
+            Refresh();
+            txtDato.Clear();
+            g = this.CreateGraphics();
+            valor = mi_Arbol.seleccionar(g, this.Font, Brushes.White, Pens.Black, mi_Arbol.Raiz, e.Location);
+            if (valor != 100)
+            {
+                txtDato.Text = valor.ToString();
+                
+            }
+        }
+
+        private void txtDato_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back) && (e.KeyChar != '-'))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtDato_TextChanged(object sender, EventArgs e)
+        {
+            //Refresh();
+            //if (txtDato.Text != String.Empty && txtDato.Text != "-")
+            //{
+            //    try
+            //    {
+            //        int num;
+            //        num = Convert.ToInt32(txtDato.Text);
+            //        if (num > 99 || num < -99)
+            //        {
+            //            txtDato.BackColor = Color.Orange;
+            //            btnInsertar.Enabled = false;
+            //            btnEliminar.Enabled = false;
+            //            btnBuscar.Enabled = false;
+            //            MessageBox.Show("No se admiten numeros tan grandes", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //        }
+            //        else
+            //        {
+            //            if (mi_Arbol.existe(num, mi_Arbol.Raiz))
+            //            {
+            //                g = this.CreateGraphics();
+            //                btnInsertar.Enabled = false;
+            //                btnEliminar.Enabled = true;
+            //                btnBuscar.Enabled = false;
+            //                mi_Arbol.resaltar(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, mi_Arbol.Raiz, num);
+            //                txtDato.BackColor = Color.Red;
+            //            }
+            //            else
+            //            {
+            //                btnInsertar.Enabled = true;
+            //                btnEliminar.Enabled = false;
+            //                btnBuscar.Enabled = true;
+            //                txtDato.BackColor = Color.Green;
+            //            }
+            //        }
+            //    }
+            //    catch
+            //    {
+            //        MessageBox.Show("No se admiten numeros tan grandes 2", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //        txtDato.BackColor = Color.Orange;
+            //    }
+            //}
+            //else
+            //{
+            //    txtDato.BackColor = Color.Red;
+            //    btnInsertar.Enabled = false;
+            //    btnEliminar.Enabled = false;
+            //    btnBuscar.Enabled = false;
+            //}
+        }
+    }
+}
+
